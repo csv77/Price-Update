241df5d673d8d46cd68c276fcc7f9d51728798a5
vojnovitscs <vojnovitscs@Vojnovits-Ntbk.foresth.hu>
2018-01-24 11:10:03 +0100

    commit Price Update

--- src/main/java/com/mycompany/priceupdate/SourceWorkbook.java
+++ src/main/java/com/mycompany/priceupdate/SourceWorkbook.java
@@ -111,6 +111,9 @@ public class SourceWorkbook {
             List<Cell> listOfCellsOfSchema = new ArrayList<>();
             for(Columns column : columnsForSchema) {
                 if(column.ordinal() < lastColumn) {
+                    if(row.getCell(column.ordinal()).getCellTypeEnum().equals(CellType.NUMERIC) && row.getCell(column.ordinal()).getNumericCellValue() == 0) {
+                        continue;
+                    }
                     if(row.getCell(column.ordinal()).toString().equals("")) {
                         continue;
                     }
